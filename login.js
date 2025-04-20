947cd508a0c73ef6bb76e373e0581d049e3be232
Nitesh Kumar Vashisht <nitesh@shooliniuniversity.com>
2025-04-20 10:56:14 +0530

    Update login.js

--- login.js
+++ login.js
@@ -1,12 +1,9 @@
-// login.js
 function login() {
-  const CLIENT_ID = "c2a56c8619af49fea91a155f4d32752b"; // your client ID
-const REDIRECT_URI = "https://niteshkumarvashisht.github.io/spotifyclock/auth.html";
-const SCOPES = "streaming user-read-email user-read-private user-modify-playback-state user-read-playback-state";
+  const CLIENT_ID = "c2a56c8619af49fea91a155f4d32752b";
+  const REDIRECT_URI = "https://niteshkumarvashisht.github.io/spotifyclock/auth.html";
+  const SCOPES = "streaming user-read-email user-read-private user-modify-playback-state user-read-playback-state";
 
-const loginUrl = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=token&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&scope=${encodeURIComponent(SCOPES)}`;
+  const loginUrl = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=token&redirect_uri=${encodeURIComponent(REDIRECT_URI)}&scope=${encodeURIComponent(SCOPES)}`;
 
-window.location.href = loginUrl;
-
-  }
-  
+  window.location.href = loginUrl;
+}
